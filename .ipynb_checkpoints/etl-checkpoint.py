ebc65eda1eb8f4a2b66080966c882b161e044e7f
David <amoat7@gmail.com>
2020-06-24 13:02:11 +1000

    update

--- .ipynb_checkpoints/etl-checkpoint.py
+++ .ipynb_checkpoints/etl-checkpoint.py
@@ -4,18 +4,34 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Loads data from s3 into the staging tables.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data from the staging tables into the analysis tables.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    - Establishes connection to redshift and gets cursor to it.  
+    
+    - Loads data from s3 into the staging tables. 
+    
+    - Insert data from the staging tables into the analysis tables.
+    
+    - Finally, closes the connection. 
+    """
+    
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
